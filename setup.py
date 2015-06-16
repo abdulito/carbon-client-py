a4d45befd800be23018af12afa9050176f96626b
abdul <abdul@objectlabs.com>
2015-06-16 13:48:35 -0700

    rename carbon-client to carbonio-client

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='carbon_client',
+    name='carbonio_client',
     version='0.0.1',
     packages=find_packages(),
     install_requires=[
