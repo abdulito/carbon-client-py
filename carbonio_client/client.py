b4d8b126b02ae5bd9e6ae85b99ed93a6be99d314
abdul <abdul@objectlabs.com>
2016-07-04 15:12:21 -0700

    stuff status code

--- carbonio_client/client.py
+++ carbonio_client/client.py
@@ -88,7 +88,8 @@ class CarbonIOClient(Endpoint):
                 logger.error("Failed to parse response to json. Raw response:\n****%s\n******" % response.text)
                 raise
         else:
-            raise FetchUrlError("Error (%s): %s" % (response.status_code, response.json()))
+            raise FetchUrlError("Error (%s): %s" % (response.status_code, response.json()),
+                                status_code=response.status_code)
 
     ####################################################################################################################
     def _setup_authentication(self, authentication):
