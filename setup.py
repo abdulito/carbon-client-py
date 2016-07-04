9f05ff1c8dec78863e1a5b6d0c55033b6438b799
abdul <abdul@objectlabs.com>
2016-07-03 18:10:22 -0700

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='carbonio_client',
-    version='0.0.6',
+    version='0.0.7',
     packages=find_packages(),
     install_requires=[
         'httplib2>=0.9'
